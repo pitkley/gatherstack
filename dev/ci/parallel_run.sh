354ff552016515a1e6c25035a499893b1c12ccaa
Pit Kleyersburg <pitkley@googlemail.com>
2024-09-15 17:04:04 +0200

    Libre: ensure build fails quickly

--- dev/ci/parallel_run.sh
+++ dev/ci/parallel_run.sh
@@ -18,7 +18,7 @@ trap "rm -rf $log_file" EXIT
 # Remove parallel citation log spam.
 echo 'will cite' | parallel --citation &>/dev/null
 
-parallel --jobs 4 --memfree 500M --keep-order --line-buffer --joblog "$log_file" -v "$@"
+parallel --halt now,fail=1 --jobs 4 --memfree 500M --keep-order --line-buffer --joblog "$log_file" -v "$@"
 code=$?
 
 echo "--- done - displaying job log:"
